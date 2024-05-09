63c7c46fcc8366504946ccecdeff4f6f30be888c
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-09 22:07:50 +0530

    dns records added

--- roboshop.tf
+++ roboshop.tf
@@ -9,6 +9,15 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z05459522TM73CF1WNKI7"
+  name    = "frontend.jdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
@@ -20,6 +29,19 @@ resource "aws_instance" "mongodb" {
   }
 }
 
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z05459522TM73CF1WNKI7"
+  name    = "mongodb.jdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
+
+
+
+
 resource "aws_instance" "catalogue" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
@@ -29,4 +51,13 @@ resource "aws_instance" "catalogue" {
   tags = {
     Name = "catalogue"
   }
+}
+
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z05459522TM73CF1WNKI7"
+  name    = "catalogue.jdevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
 }
