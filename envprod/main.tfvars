d9dfbb3300b809daa1f9a9039276b1ec277a3d8e
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-11 17:15:24 +0530

    okok

---
+++ envprod/main.tfvars
@@ -0,0 +1,1 @@
+test = "hello this is dev variable in tfvars"
--- roboshop.tf
+++ roboshop.tf
@@ -1,58 +1,11 @@
-variable "ami" {
-  default = "ami-0f3c7d07486cad139"
+terraform {
+  backend "s3" {}
 }
 
-variable "instance_type" {
-  default = "t2.micro"
-}
 
-variable "vpc_security_group_ids" {
-  default = ["sg-0490a4409ed09ba21"]
-}
+variable "test" {}
 
-variable "zone_id" {
-  default = "Z05459522TM73CF1WNKI7"
+output "name" {
+  value = var.test
   
-}
-
-variable "components" {
-
-  default = {
-      frontend  = {},
-      mongodb   = {},
-      catalogue = {},
-      redis     = {},
-      user      = {},
-      cart      = {},
-      mysql     = {},
-      shipping  = {},
-      rabbitmq  = {},
-      payment   = {}
-
-  }
-
-}
-
-resource "aws_instance" "instance" {
-
-  for_each = var.components
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = var.vpc_security_group_ids
-
-  tags = {
-    Name = each.key
-  }
-}
-
-
-
-resource "aws_route53_record" "record" {
-  for_each = var.components
-  zone_id = var.zone_id
-  name    = "${each.key}.jdevops.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance[each.key].private_ip]
 }
